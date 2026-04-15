b57819a4155cd9574ef40d70e6f7181e01abd854
karnikachoudhury <karnika.choudhury@gmail.com>
2026-04-15 11:45:48 -0500

    updated code

--- code.lisp
+++ code.lisp
@@ -1,81 +1,94 @@
 ; structure verifiers
 (defun pathp (p)
-        (if (endp p)
-            t
-        (and (symbolp (car p))
-            (pathp (cdr p)))))
-    (defun filep (x)
-        (and (consp x)
-            (equal (car x) :file)
-            (consp (cdr x))
-            (null (cddr x))))
+  (if (endp p)
+      t
+    (and (symbolp (car p))
+         (pathp (cdr p)))))
 
-    (mutual-recursion
-    
-        (defun dirp (x)
-            (declare (xargs :measure (acl2-count x)))
-            (and (consp x)
-                (equal (car x) :dir)
-                (consp (cdr x))
-                (null (cddr x))
-                (entriesp (cadr x))))
+(defun filep (x)
+  (and (consp x)
+       (equal (car x) :file)
+       (consp (cdr x))
+       (null (cddr x))))
 
-            (defun entryp (x)
-                (declare (xargs :measure (acl2-count x)))
-                (and (consp x)
-                    (symbolp (car x))
-                    (or (filep (cdr x))
-                    (dirp (cdr x)))))
+(mutual-recursion
 
-            (defun entriesp (entries)
-                (declare (xargs :measure (acl2-count entries)))
-                (if (endp entries)
-                    t
-                (and (entryp (car entries))
-                    (entriesp (cdr entries))))))
+ (defun dirp (x)
+   (declare (xargs :measure (acl2-count x)))
+   (and (consp x)
+        (equal (car x) :dir)
+        (consp (cdr x))
+        (null (cddr x))
+        (entriesp (cadr x))))
 
-    (defun nodep (x)
-            (or (filep x)
-                (dirp x)))  
-    ; functions 
+ (defun entryp (x)
+   (declare (xargs :measure (acl2-count x)))
+   (and (consp x)
+        (symbolp (car x))
+        (or (filep (cdr x))
+            (dirp (cdr x)))))
 
-    (defun lookup-name (name entries)
-        (if (endp entries)
-            nil
-            (if (equal name (caar entries))
-                (cdar entries)
-            (lookup-name name (cdr entries)))))
+ (defun entriesp (entries)
+   (declare (xargs :measure (acl2-count entries)))
+   (if (endp entries)
+       t
+     (and (entryp (car entries))
+          (entriesp (cdr entries))))))
 
-    (defun put-name (name node entries)
-        (if (endp entries)
-            (list (cons name node))
-        (if (equal name (caar entries))
-            (cons (cons name node)
+(defun nodep (x)
+  (or (filep x)
+      (dirp x)))
+
+(defun valid-fs (fs)
+  (dirp fs))
+
+
+; helper functions
+(defun lookup-name (name entries)
+  (if (endp entries)
+      nil
+    (if (equal name (caar entries))
+        (cdar entries)
+      (lookup-name name (cdr entries)))))
+
+(defun put-name (name node entries)
+  (if (endp entries)
+      (list (cons name node))
+    (if (equal name (caar entries))
+        (cons (cons name node)
               (cdr entries))
-            (cons (car entries)
-                (put-name name node (cdr entries))))))
-    (defun lookup-path (p fs)
-        (if (endp p)
-            fs
-            (if (dirp fs)
-                (lookup-path (cdr p)
-                     (lookup-name (car p) (cadr fs))) nil)))
-    (defun insert-path (p newnode fs)
-        (if (or (endp p) (not (dirp fs)))
-            fs
-            (if (endp (cdr p))
-                (list :dir
-                    (put-name (car p) newnode (cadr fs)))
-                (let ((child (lookup-name (car p) (cadr fs))))
-                    (if (dirp child)
-                    (list :dir
-                        (put-name (car p)(insert-path (cdr p) newnode child)
-                        (cadr fs))) fs)))))
-    (defun valid-fs (fs)
-        (dirp fs))
+      (cons (car entries)
+            (put-name name node (cdr entries))))))
 
-    ; theorem to be proved
-    (DEFTHM FINAL-PROJECT
-        (IMPLIES (AND (VALID-FS FS) (PATHP P) (NODEP V))
-           (VALID-FS (INSERT-PATH P V FS)))
-        :INSTRUCTIONS (:PROVE))            
+
+; filesystem operations
+(defun lookup-path (p fs)
+  (if (endp p)
+      fs
+    (if (dirp fs)
+        (lookup-path (cdr p)
+                     (lookup-name (car p) (cadr fs)))
+      nil)))
+
+(defun insert-path (p name newnode fs)
+  (if (not (dirp fs))
+      fs
+    (if (endp p)
+        (list :dir
+              (put-name name newnode (cadr fs)))
+      (let ((child (lookup-name (car p) (cadr fs))))
+        (if (dirp child)
+            (list :dir
+                  (put-name (car p)
+                            (insert-path (cdr p) name newnode child)
+                            (cadr fs)))
+          fs)))))
+
+; theorem
+(defthm final-project
+  (implies (and (valid-fs fs)
+                (pathp p)
+                (symbolp name)
+                (nodep v))
+           (valid-fs (insert-path p name v fs)))
+  :instructions (:prove))
